e0bf056d7375099c67f94511fc2ad9059e41cbe3
Sam Lantinga <slouken@libsdl.org>
2000-11-04 01:24:19 +0000

    Updated to detect multiple binaries

--- detect/detect.sh
+++ detect/detect.sh
@@ -28,35 +28,42 @@ if [ ! -f "$product.md5" ]; then
     echo "No MD5 checksum file for $product" >&2
     exit 2
 fi
-product_bin=`tail +1 "$product.md5" | head -n 1`
 product_desc=`tail +2 "$product.md5" | head -n 1`
 product_url=`tail +3 "$product.md5" | head -n 1`
-for path in "$UPDATE_CWD" /opt /opt/games /usr/games /usr/local/games "$HOME/games" "$HOME"
-do if [ ! -d "$path" ]; then
-       continue
-   fi
-   binary=`ls -d "$path/$product_bin" 2>/dev/null | head -n 1`
-   if [ ! -f "$binary" ]; then
-       binary=`ls -d "$path"/*/"$product_bin" 2>/dev/null | head -n 1`
-   fi 
-   if [ -f "$binary" ]; then
-       product_path=`dirname $binary`
-       if [ -w "$product_path" ]; then
-           set -- `"$md5" "$binary"`
-           sum="$1"
-           tail +4 "$product.md5" | while read line
-           do set -- $line
-               if [ "$sum" = "$1" ]; then
-                   # Woohoo!  We found it!
-                   product_version=$2
-                   echo "$product_version"
-                   echo "$product_desc"
-                   echo "$product_path"
-                   echo "$product_url"
-                   exit 0
-               fi
-           done
-       fi
-   fi
+for product_bin in `tail +1 "$product.md5" | head -n 1`
+do  for path in "$UPDATE_CWD" \
+                /opt \
+                /opt/games \
+                /usr/games \
+                /usr/local/games \
+                "$HOME/games" \
+                "$HOME"
+    do  if [ ! -d "$path" ]; then
+            continue
+        fi
+        binary=`ls -d "$path/$product_bin" 2>/dev/null | head -n 1`
+        if [ ! -f "$binary" ]; then
+            binary=`ls -d "$path"/*/"$product_bin" 2>/dev/null | head -n 1`
+        fi 
+        if [ -f "$binary" ]; then
+            product_path=`dirname $binary`
+            if [ -w "$product_path" ]; then
+                set -- `"$md5" "$binary"`
+                sum="$1"
+                tail +4 "$product.md5" | while read line
+                do  set -- $line
+                    if [ "$sum" = "$1" ]; then
+                        # Woohoo!  We found it!
+                        product_version=$2
+                        echo "$product_version"
+                        echo "$product_desc"
+                        echo "$product_path"
+                        echo "$product_url"
+                        exit 0
+                    fi
+                done
+            fi
+        fi
+    done
 done
 exit 2
